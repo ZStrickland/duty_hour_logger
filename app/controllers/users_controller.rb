60d9157e7ee99e4404c41a8bd3aab79ba4862ebe
Zach Strickland <zach@strickland.net>
2013-10-16 00:14:45 -0400

    Minor changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
 		if @user.save
 			sign_in @user
 			flash[:success] = "Account Created"
-			redirect_to @user
+			redirect_to root_url
 		else
 			render 'new'
 		end
